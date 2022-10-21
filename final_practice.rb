4aebfab561f7a4cc3827402827c0d115ccb7444b
Hady Matar <hadyematar@gmail.com>
2022-10-20 20:25:14 -0600

    Complete Exercise 3

--- final_practice.rb
+++ final_practice.rb
@@ -1,13 +1,15 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
 
-def greeting(name)
-    "Why Hello, #{name}, how do you do?"
+def greeting 
+    "Assalamwalaykumwalaykumalsalam"
 end 
 
-
+puts greeting
 
 # What is the return value of your method?
+
+# The return value 
 # How many arguments did you pass your method?
 
 
@@ -15,6 +17,11 @@ end
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
 
+def custom_greeting(name)
+    "Why Hello, #{name}, how do you do?"
+end  
+
+# puts custom_greeting("Jacob")
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
@@ -25,7 +32,11 @@ end
 
 #3: Write a method named square that takes in one number, and returns the square of that number
 
+def square(int1)
+    int1 * int1
+end 
 
+puts square(6)
 
 # What is the return value of your method?
 # How many arguments did you pass your method?
