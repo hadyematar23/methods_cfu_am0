6824e7ae12429def34632ddfd25471d2a1c6fe19
Hady Matar <hadyematar@gmail.com>
2022-10-23 12:03:46 -0600

    Complete assignment

--- final_practice.rb
+++ final_practice.rb
@@ -9,10 +9,11 @@ puts greeting
 
 # What is the return value of your method?
 
-# The return value 
+# The return value is the string that is written between def and end, when it defines for the method what exactly to do and what it will be doing. 
+
 # How many arguments did you pass your method?
 
-
+# There are no arguments passed through this method. We are just calling the method and it returns the string. 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
@@ -24,9 +25,14 @@ end
 # puts custom_greeting("Jacob")
 
 # What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
 
+# The return method is the actual greeting with the argument (in this case, a name) interpolated into it. 
+
+# How many arguments did you pass your method?
+# One. 
+
+# What data type was your argument(s)?
+# A string. 
 
 
 
@@ -46,6 +52,14 @@ puts square(6)
 
 
 #4: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
+
+def greet_person(first_name, middle_name, last_name)
+    "#{first_name} #{middle_name} #{last_name}"
+end
+
+puts greet_person("John", "Fitzgerald", "Kennedy")
+
+
 
 
 
